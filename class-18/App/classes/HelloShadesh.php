0fce2502037474dd6245b568b3f3ab9c9c346c9a
Md Naimul Hasan Shadesh <96987942+mdshadesh@users.noreply.github.com>
2023-03-23 00:19:06 +0600

    Add files via upload

---
+++ class-18/App/classes/HelloShadesh.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\classes;
+
+class HelloShadesh
+{
+    public $message;
+
+    public function index()
+    {
+        echo $this->message = 'Hello Shadesh';
+    }
+}
---
+++ class-18/index.php
@@ -0,0 +1,6 @@
+<?php
+
+    require_once 'App/classes/HelloShadesh.php';
+    $helloshadesh = new \App\classes\HelloShadesh();
+        $helloshadesh->index();
+
